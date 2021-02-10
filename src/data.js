b098b7f19eec4e8ae85de86b1eac725181455079
Chaitali Mahida <chaitalimahida@Chaitalis-MacBook-Air.local>
2021-02-10 14:01:49 -0500

    add UseState project example

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export default [
+    {
+        id:1,
+        name: 'Jackky',
+        age:23,
+        image:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+        id:2,
+        name: 'Alexa',
+        age:22,
+        image:"https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+        id:3,
+        name: 'Elona',
+        age:25,
+        image:"https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+        id:4,
+        name: 'Eilish',
+        age:26,
+        image:"https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+        id:5,
+        name: 'Fogg',
+        age:28,
+        image:"https://images.unsplash.com/photo-1541647376583-8934aaf3448a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+        id:6,
+        name: 'Jini',
+        age:20,
+        image:"https://images.unsplash.com/photo-1589571894960-20bbe2828d0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    },
+
+]
